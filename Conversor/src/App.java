afa948a144b9670fee323c757f013c6dc5f9ae2f
Angel <villorinaangelandres@gmail.com>
2025-03-26 15:04:23 +0100

    commit con los cambios en la clase CSV y una preuba de dicho metodo en el main

--- Conversor/src/App.java
+++ Conversor/src/App.java
@@ -1,5 +1,75 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+
 public class App {
+
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int opcion;
+        
+        
+        do {
+
+            System.out.println(Menu1());
+            
+            opcion = sc.nextInt();
+
+
+            switch (opcion) {
+                
+                case 1 -> {System.out.println("Caso 1");}
+                case 2 -> System.out.println("Saliendo del programa...");   
+                default -> System.out.println("Opcion no valida");
+            }
+
+            
+        } while (opcion != 2);
+
+    ArrayList<HashMap<String, String>> ficheroPalEjemplo = new ArrayList<>();
+
+
+    HashMap<String, String> fila1 = new HashMap<>();
+    fila1.put("nombre", "angel");
+    fila1.put("edad", "21");
+    fila1.put("Ciudad", "merida");
+
+
+    HashMap<String, String> fila2 = new HashMap<>();
+    fila2.put("nombre", "lucia");
+    fila2.put("Edad", "29");
+    fila2.put("ciudad", "merida");
+
+
+    ficheroPalEjemplo.add(fila1);
+    ficheroPalEjemplo.add(fila2);
+
+
+    Csv.escribirCSV(ficheroPalEjemplo);
+
+
     }
+
+
+    public static String Menu1(){
+        String tAmarillo = "\u001B[33m";
+        String reset = "\u001B[0m";
+        String menu= tAmarillo +"----------------------------------------------------------\n"
+                            + "|                                                        |\n"
+                            + "|  "+"       Bienvenido a tu conversor de formatos" + reset + tAmarillo+"          |\n"
+                            + "|                                                        |\n"
+                            + "|--------------------------------------------------------|\n"
+                            + "|      1. Seleccionar carpeta                            |\n" 
+                            + "|      2. Salir                                          |\n"
+                            + "|                                                        |\n"
+                            + "----------------------------------------------------------\n"
+
+                            + "Contenido de la carpeta seleccionada: " 
+                            + "\n\nRuta de la carpeta seleccionada: "
+                            + "\n\nElija una opción:\n" + reset;
+        return menu;
+    } 
+
+    
 }
--- Conversor/src/Csv.java
+++ Conversor/src/Csv.java
@@ -1,3 +1,39 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Csv {
-    
+
+    private static final String nombre = "ficheroCSV.csv";
+
+    public static void escribirCSV(ArrayList<HashMap<String, String>> fichero) {
+        File file = new File(nombre);
+        String path = file.getAbsolutePath();
+        
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+            ArrayList<String> cabeceras = new ArrayList<>(fichero.get(0).keySet());
+            
+            bw.write(String.join(",", cabeceras));
+            bw.newLine();
+            
+            for (HashMap<String, String> fila : fichero) {
+                List<String> valores = new ArrayList<>();
+                for (String cabecera : cabeceras) {
+                    valores.add(fila.getOrDefault(cabecera, ""));
+                }
+                bw.write(String.join(",", valores));
+                bw.newLine();
+            }
+            System.out.println("CSV creado exitosamente en: " + path);
+        } catch (IOException e) {
+            System.err.println("Error al escribir: " + e.getMessage());
+        }
+    }
 }
+
+
+
