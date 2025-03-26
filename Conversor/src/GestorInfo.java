621c1fa24d87cb7728bf23ee734da4cc2169a132
lfernandezf11 <lfernandezf11@iesalbarregas.es>
2025-03-26 14:15:43 +0100

    crud para el gestor (sin revisar)

--- Conversor/src/GestorInfo.java
+++ Conversor/src/GestorInfo.java
@@ -8,23 +8,60 @@ public GestorInfo(){
     items = new ArrayList<HashMap<String, String>>();
 }
 
-public void addItem(HashMap<String, String> item){
+public boolean addItem(HashMap<String, String> item){
     try {
         items.add(item);
+        return true;
     } catch (Exception e) {
         System.err.println("Error" + e.getMessage());
+        return false;
     }
 }
 
 public int searchByIndex(int index){
+    int buscar = -1;
     try {
-        int buscar = -1;
         for(int i=0; i<items.size(); i++){
-            
+            if(items.indexOf(items.get(i)) == index){
+                buscar = i;
+            }
         }
     } catch (Exception e) {
-        // TODO: handle exception
+        System.err.println("Error en la operación de búsqueda: " + e.getMessage());
+    }return buscar;
+}
+
+public boolean modifyItem(int index, HashMap<String, String> map){
+    if(searchByIndex(index) != -1){
+        try {
+            items.set(index, map);
+            return true;
+        } catch (Exception e) {
+            System.err.println("Error en la operación de modificación: " + e.getMessage());
+        }
     }
 }
 
+public boolean deleteItem(int index){
+    if(searchByIndex(index) != -1){
+        try {
+            items.remove(index);
+            return true;
+        } catch (Exception e) {
+            System.err.println("Error en la operación de borrado: " + e.getMessage());
+        }
+    }
 }
+
+
+
+
+@Override
+public String toString(List<HashMap <String, String>>){
+
+}
+
+
+}
+
+
