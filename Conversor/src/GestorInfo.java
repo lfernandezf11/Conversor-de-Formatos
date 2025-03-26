b29e46579e5e9b7a23ecd09fd0730718172ca049
lfernandezf11 <lfernandezf11@iesalbarregas.es>
2025-03-26 13:04:33 +0100

    Clase gestora inicial

--- Conversor/src/GestorInfo.java
+++ Conversor/src/GestorInfo.java
@@ -1,3 +1,30 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 public class GestorInfo {
-    
+    List<HashMap <String, String>> items;
+
+public GestorInfo(){
+    items = new ArrayList<HashMap<String, String>>();
+}
+
+public void addItem(HashMap<String, String> item){
+    try {
+        items.add(item);
+    } catch (Exception e) {
+        System.err.println("Error" + e.getMessage());
+    }
+}
+
+public int searchByIndex(int index){
+    try {
+        int buscar = -1;
+        for(int i=0; i<items.size(); i++){
+            
+        }
+    } catch (Exception e) {
+        // TODO: handle exception
+    }
+}
+
 }
